95dfb1cefe58a66524a12b6d542b04418868ca83
sekhar1926 <adisekharraju@gmail.com>
2018-10-30 16:19:26 -0500

    commit

--- Icp10/Source/Vote/client/src/app/vote.ts
+++
@@ -1,9 +0,0 @@
-export class Vote{
-    _id: string;
-    user_id: string;
-    vote_title: string;
-    option1: string;
-    option2: string;
-    option3: string;
-    option4: string;
-}
---
+++ Icp10/Source/icp10/app.js
@@ -0,0 +1,79 @@
+var express = require("express");
+var app = express();
+var port = process.env.PORT || 3000;
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+var mongoose = require("mongoose");
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://sekhar1926:Aditya*1996@ds227243.mlab.com:27243/ase",{
+   
+    useNewUrlParser: true,
+});
+
+
+var nameSchema = new mongoose.Schema({
+    id: String,
+    name: String,
+    course: String,
+    major: String
+});
+var User = mongoose.model("User", nameSchema);
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/addname", (req, res) => {
+    var myData = new User(req.body);
+    myData.save()
+        .then(item => {
+            res.send("Name saved to database");
+        })
+        .catch(err => {
+            res.status(400).send("Unable to save to database");
+        });
+});
+
+app.get('/getusers',(req,res)=>{
+        let search_text = req.query.major;
+        console.log(search_text);
+        User.find({major:{ $regex: search_text, $options: 'i' }}).exec((err, result) => {
+            if (!err) {
+                
+                res.write("<html><head></head><body><h2>Search results for  ");
+                res.write(search_text);
+                res.write("</h1><table><tr><th>ID</th><th>Name</th><th>course</th><th>major</th></tr>");
+                
+                for(var i=0;i<result.length;i++){
+                    res.write("<tr><td>");
+                    res.write(result[i].id);
+                    res.write("</td>");
+                    res.write("<td>");
+                    res.write(result[i].name);
+                    res.write("</td>");
+                    res.write("<td>");
+                    res.write(result[i].course);
+                    res.write("</td>");
+                    res.write("<td>");
+                    res.write(result[i].major);
+                    res.write("</td></tr>");
+                }
+                res.write("</table></body></html>");
+                res.end();
+                //alert(result)
+                console.log(result);
+            } else {
+                res.send({
+                    result: "Failure",
+                    message: "Error in fetching students list",
+                    error: err
+                });
+            }
+        });
+});
+
+app.listen(port, () => {
+    console.log("Server listening on port " + port);
+});
